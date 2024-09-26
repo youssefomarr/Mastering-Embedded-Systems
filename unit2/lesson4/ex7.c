90c4724a9e983c439946e0c21942f4c86746b184
youssefomarr <youssef.zamalkawy@gmail.com>
2024-09-26 18:17:55 +0300

    o

---
+++ unit2/lesson4/ex7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "string.h"
+int main()
+{
+ char text [100];
+ int i,count;
+ printf("Enter a string \n");
+ scanf("%s",text);
+    for(i=0;text[i]!='\0';i++);
+        
+   
+    
+    printf("length of the string: %d",i);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
